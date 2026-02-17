f384d60c981057bbb78bfe8b01dfd7c3fbf0fc33
Luiz Alberto <luizqsinclusao@gmail.com>
2026-02-17 11:35:10 -0300

    Fix global Select component arrow icon size

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,4 +1,5 @@
 import React, { SelectHTMLAttributes, forwardRef } from 'react';
+import { ChevronDown } from 'lucide-react';
 
 export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     label?: string;
@@ -42,10 +43,8 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(({
                 >
                     {children}
                 </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-400">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-400">
+                    <ChevronDown size={16} />
                 </div>
             </div>
             {error && (
